7cc8a4c3e6e8eea371bb474e4b44a6fa795e5d4b
globbertot <95583040+globbertot@users.noreply.github.com>
2024-10-01 19:04:33 +0300

    Update youtube.h 0.0.1

--- youtube.h
+++ youtube.h
@@ -7,8 +7,17 @@
 #include <QStandardPaths>
 #include <QDebug>
 #include <QFile>
+#include <QDir>
 #include <QStandardPaths>
 #include <QDesktopServices>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTimer>
+#include <QString>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+
+#include "qmediaplayerwidget.h"
 
 class Youtube : public QObject
 {
@@ -16,18 +25,27 @@ class Youtube : public QObject
 public:
     explicit Youtube(QObject *parent = nullptr);
 
-    void download(const QUrl& url, const QString& outFolder);
+    void download(const QUrl& url, const QString& outFolder, const bool& openInITunes = false, const bool& openInDefault = false);
 private slots:
+    void startDownload(const QUrl& url, const QString& outFolder, const bool& openInItunes, const bool& openInDefault);
     void onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus);
-    void onProcessErrorOccurred(QProcess::ProcessError error);
+    void onProcessErrorOccurred(QProcess::ProcessError err);
 
 signals:
     void complete();
+    void info(const QString& msg, const float& timeout = 3.0);
+    void error(const QString& msg, const float& timeout = 3.0);
 
 private:
-    QString path = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation) + "/youtube-dl";
-    QProcess* prc;
+    QString getFileName(const QUrl& url, const QString& outFolder);
 
+    QMediaPlayer* player;
+    QAudioOutput* audio;
+    QString path = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation) + "/yt-dlp";
+    QString outPath;
+    bool iTunes;
+    bool openSong;
+    QProcess* prc;
 };
 
 #endif // YOUTUBE_H
